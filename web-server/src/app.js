dc8e2e494545fabe619ee22347e0ee7f155f58b9
Roy Hevrony <roy.hevrony@gmail.com>
2021-10-02 10:05:42 +0300

    before dymanic pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -9,24 +9,6 @@ const app = express()
 // making sure that index.html is loaded in the root.
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'Roy',
-        age: 27
-    }, {
-        name: 'May',
-        age: 26
-    }])
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>Welcome to about page</h1>')
-})
-
 app.get('/weather', (req, res) => {
     res.send({
         temparature: 30,
