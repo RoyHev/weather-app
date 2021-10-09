88879b14c330740f0a833a79026cc1d511f0d2f0
Roy Hevrony <roy.hevrony@gmail.com>
2021-10-09 10:51:43 +0300

    styling

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,20 +1,33 @@
 console.log("Javascript clientside file loaded")
+// fetch('https://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
 
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+// Targetting by classname is just '.className'
+// Targetting by id
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather?address=boston+usa').then((response) => {
-    response.json().then((data)=>{
-        if (data.error){
-            console.log("Error")
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-    
-        }
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ''
+    const url = 'http://localhost:3000/weather?address=' + location
+    fetch(url).then((response) => {
+        response.json().then((data)=>{
+            messageOne.textContent = ''
+            if (data.error){
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = "Currently " + data.forecast.description + ". It is " +  data.forecast.temperature + " degrees. Feels like: " + data.forecast.feelslike
+            }
 
+        })
     })
 })
